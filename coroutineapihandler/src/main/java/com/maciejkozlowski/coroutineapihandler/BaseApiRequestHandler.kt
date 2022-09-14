142a771635e3ccd3a085cbb9fe438fa45aaa97b3
Maciej Kozlowski <mac229@interia.eu>
2022-09-14 13:07:49 +0200

    Updated base api request handler

--- coroutineapihandler/src/main/java/com/maciejkozlowski/coroutineapihandler/BaseApiRequestHandler.kt
+++ coroutineapihandler/src/main/java/com/maciejkozlowski/coroutineapihandler/BaseApiRequestHandler.kt
@@ -2,11 +2,6 @@ package com.maciejkozlowski.coroutineapihandler
 
 import retrofit2.HttpException
 import java.io.IOException
-import java.net.HttpURLConnection
-
-/**
- * Created by Maciej Kozłowski on 2019-10-23.
- */
 
 abstract class BaseApiRequestHandler<ERROR> {
 
@@ -39,31 +34,14 @@ abstract class BaseApiRequestHandler<ERROR> {
 
     private fun mapToApiError(throwable: Throwable): ERROR =
         when (throwable) {
-            is HttpException -> handleHttpException(throwable)
+            is HttpException -> createRequestError(throwable)
             is IOException   -> createConnectionError()
             else             -> createUnknownError(throwable)
         }
 
-    private fun handleHttpException(exception: HttpException): ERROR =
-        when (exception.code()) {
-            in (400 until 500) -> handleBadRequest(exception)
-            in (500 until 600) -> createServerError()
-            else               -> createUnknownError(exception)
-        }
-
-    private fun handleBadRequest(exception: HttpException): ERROR =
-        when (exception.code()) {
-            HttpURLConnection.HTTP_UNAUTHORIZED -> createUnauthorizedError()
-            else                                -> createRequestError(exception)
-        }
-
-    protected abstract fun createUnauthorizedError(): ERROR
-
     protected abstract fun createConnectionError(): ERROR
 
     protected abstract fun createRequestError(exception: HttpException): ERROR
-
-    protected abstract fun createServerError(): ERROR
 
     protected abstract fun createUnknownError(throwable: Throwable): ERROR
 }
